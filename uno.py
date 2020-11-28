35597aefe96690d0d19ae4bcef923913572a0962
Michał Antos <antosak47@gmail.com>
2020-11-28 11:10:41 +0100

    class Client

--- uno.py
+++ uno.py
@@ -5,3 +5,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 print("Hello world")
+
+
+def create_new_world():
+    pass
+
+
+class Client(object):
+    def __init__(self, price, coordinates, prep_time, teaching_time, hour):
+        self.price = price
+        self.coordinates = coordinates
+        self.prep_time = prep_time
+        self.teaching_time = teaching_time
+        self.hour = hour
+
+    def add_client(self):
+        pass
+
+    def remove_client(self):
+        pass
+
