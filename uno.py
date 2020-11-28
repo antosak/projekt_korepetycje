5ab4f30997c8a420de1dbc93d7a08139df8fd21f
NiePamietam <49474129+NiePamietam@users.noreply.github.com>
2020-11-28 13:43:25 +0100

    Legalność dziecka i kappa

--- uno.py
+++ uno.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from math import sqrt
 import matplotlib.pyplot as plt
 import pandas as pd
 import random
@@ -48,6 +49,64 @@ class Client(object):
 
     def remove_client(self):
         pass
+
+
+def legal_child(Client_list, child, home_returns_vector) -> bool:
+    income = 0
+    for i in range(0, len(child)):
+        if child[i] == 1:
+            income += Client_list[i].price
+    if income > 350:
+        return False
+    coordinate_list = []
+    for i in range(0, len(child)):
+        if child[i] == 1:
+            if Client_list[i].coordinates in coordinate_list:
+                return False
+            else:
+                coordinate_list.append(Client_list[i].coordinates)
+    days_list = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+    days_value_list = [0, 0, 0, 0, 0, 0, 0]
+    last_coordinates = our_home
+    total_time = 0
+    for i in range(0, len(child)):
+        if child[i] == 1:
+            j = days_list.index(Client_list[i].day)
+            days_value_list[j] += Client_list[i].teaching_time
+            days_value_list[j] += (sqrt(((Client_list[i].coordinates[0] - last_coordinates[0]) ** 2) +
+                                        ((Client_list[i].coordinates[1] - last_coordinates[1]) ** 2)))/average_speed
+            last_coordinates = Client_list[i].coordinates
+            if home_returns_vector[i] == 1:
+                last_coordinates = our_home
+                days_value_list[j] += (sqrt(((Client_list[i].coordinates[0] - last_coordinates[0]) ** 2) +
+                                            ((Client_list[i].coordinates[1] - last_coordinates[1]) ** 2)))/average_speed
+    for days in days_value_list:
+        if days > 4:
+            return False
+        total_time += days
+    for i in range(0, len(child)):
+        if child[i] == 1:
+            total_time += Client_list[i].prep_time
+    if total_time > 28:
+        return False
+    return True
+
+
+def kappa_maker(Client_list, child):
+    kappa = []
+    for i in range(0, len(child)):
+        if child[i] == 0:
+            kappa.append(0)
+        else:
+            if i+1 != len(child):
+                if child[i+1] == 1:
+                    if Client_list[i].day == Client_list[i+1].day:
+                        kappa.append(0)
+                    else:
+                        kappa.append(1)
+            else:
+                kappa.append(1)
+    return kappa
 
 
 example_sol = (0, 1, 0, 1, 1)
