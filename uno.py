7d2514495ab75c497cd2b4615423424ba39ab0be
Michał Antos <antosak47@gmail.com>
2020-11-28 14:24:49 +0100

    objective functions

--- uno.py
+++ uno.py
@@ -5,8 +5,7 @@ import numpy as np
 from math import sqrt
 import matplotlib.pyplot as plt
 import pandas as pd
-import random
-import time
+
 
 # constants
 default_path = 'Tutoring.xlsx'
@@ -51,48 +50,48 @@ class Client(object):
         pass
 
 
-def legal_child(Client_list, child, home_returns_vector) -> bool:
+def legal_child(client_list, child, home_returns_vector) -> bool:
     income = 0
     for i in range(0, len(child)):
         if child[i] == 1:
-            income += Client_list[i].price
+            income += client_list[i].price
     if income > 350:
         return False
     coordinate_list = []
     for i in range(0, len(child)):
         if child[i] == 1:
-            if Client_list[i].coordinates in coordinate_list:
+            if client_list[i].coordinates in coordinate_list:
                 return False
             else:
-                coordinate_list.append(Client_list[i].coordinates)
+                coordinate_list.append(client_list[i].coordinates)
     days_list = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
     days_value_list = [0, 0, 0, 0, 0, 0, 0]
     last_coordinates = our_home
     total_time = 0
     for i in range(0, len(child)):
         if child[i] == 1:
-            j = days_list.index(Client_list[i].day)
-            days_value_list[j] += Client_list[i].teaching_time
-            days_value_list[j] += (sqrt(((Client_list[i].coordinates[0] - last_coordinates[0]) ** 2) +
-                                        ((Client_list[i].coordinates[1] - last_coordinates[1]) ** 2)))/average_speed
-            last_coordinates = Client_list[i].coordinates
+            j = days_list.index(client_list[i].day)
+            days_value_list[j] += client_list[i].teaching_time
+            days_value_list[j] += (sqrt(((client_list[i].coordinates[0] - last_coordinates[0]) ** 2) +
+                                        ((client_list[i].coordinates[1] - last_coordinates[1]) ** 2))) / average_speed
+            last_coordinates = client_list[i].coordinates
             if home_returns_vector[i] == 1:
                 last_coordinates = our_home
-                days_value_list[j] += (sqrt(((Client_list[i].coordinates[0] - last_coordinates[0]) ** 2) +
-                                            ((Client_list[i].coordinates[1] - last_coordinates[1]) ** 2)))/average_speed
+                days_value_list[j] += (sqrt(((client_list[i].coordinates[0] - last_coordinates[0]) ** 2) +
+                                            ((client_list[i].coordinates[1] - last_coordinates[1]) ** 2))) / average_speed
     for days in days_value_list:
         if days > 4:
             return False
         total_time += days
     for i in range(0, len(child)):
         if child[i] == 1:
-            total_time += Client_list[i].prep_time
+            total_time += client_list[i].prep_time
     if total_time > 28:
         return False
     return True
 
 
-def kappa_maker(Client_list, child):
+def kappa_maker(client_list, child):
     kappa = []
     for i in range(0, len(child)):
         if child[i] == 0:
@@ -100,7 +99,7 @@ def kappa_maker(Client_list, child):
         else:
             if i+1 != len(child):
                 if child[i+1] == 1:
-                    if Client_list[i].day == Client_list[i+1].day:
+                    if client_list[i].day == client_list[i + 1].day:
                         kappa.append(0)
                     else:
                         kappa.append(1)
@@ -114,18 +113,36 @@ example_kappa = (1, 0, 0, 0, 1)
 world = create_brave_new_world()
 
 
-def objective_function(current_solution=example_sol, kappa=example_kappa):
+def income_objective_function(solution: list = example_sol, kappa: list = example_kappa):
     income = []
     last_visit = our_home
-    kappa_counter = 0
+    counter = 0
     for elem in world:
         income.append(elem.price - np.sqrt((elem.coordinates[0] - last_visit[0])**2 + (elem.coordinates[1] - last_visit[1])**2)*fuel_coeff
-                      - kappa[kappa_counter]*np.sqrt((our_home[0] - last_visit[0])**2 + (our_home[1] - last_visit[1])**2)*fuel_coeff)
-        if current_solution[kappa_counter] == 1:
+                      - kappa[counter]*np.sqrt((our_home[0] - last_visit[0])**2 + (our_home[1] - last_visit[1])**2)*fuel_coeff)
+        if solution[counter] == 1:
             last_visit = elem.coordinates
-        kappa_counter += 1
-    result = np.dot(current_solution, income)
+        counter += 1
+    result = np.dot(solution, income)
     return result
 
 
-res = objective_function()
+def time_objective_function(solution: list = example_sol, kappa: list = example_kappa):
+    time_spent = []
+    last_visit = our_home
+    counter = 0
+    for elem in world:
+        time_spent.append(elem.prep_time + elem.teaching_time + np.sqrt((elem.coordinates[0] - last_visit[0])**2 + (elem.coordinates[1] - last_visit[1])**2)/average_speed
+                          + kappa[counter]*np.sqrt((our_home[0] - last_visit[0])**2 + (our_home[1] - last_visit[1])**2)/average_speed)
+        if solution[counter] == 1:
+            last_visit = elem.coordinates
+        counter += 1
+    result = np.dot(solution, time_spent)
+    return result
+
+
+def final_objective_function():
+    return income_objective_function()/(time_objective_function() + 1)
+
+
+res = final_objective_function()  # the more the better!
