a386ceb1497c088e418f4b4aaaa42478f8369cf7
sbrozyna11 <sbrozyna11@gmail.com>
2021-01-05 14:40:08 +0100

    Czas to pinionc

--- data_analysis.py
+++ data_analysis.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
 df500 = pd.read_excel("Dat/RawData_iter_500.xlsx", index_col=0)
 df1000 = pd.read_excel("Dat/RawData_iter_1000.xlsx", index_col=0)
@@ -15,18 +15,18 @@ df10000 = pd.read_excel("Dat/RawData_iter_10000.xlsx", index_col=0)
 fig_size = (12.8, 9.6)
 
 fig, axs = plt.subplots(2, 2, figsize=(10, 8), sharey="row", sharex="row")
-fig.suptitle("Całkowity zysk przy różnych liczbach iteracji algorytmu ", fontsize=16)
+fig.suptitle("Całkowity zysk przy różnych liczbach iteracji algorytmu 500-4000", fontsize=16)
 fig.subplots_adjust(top=0.90, left=0.1)
 axs[0, 0].hist(df500['Maximum income'], bins=20, rwidth=0.9)
 axs[0, 0].set_title("500")
-axs[0,0].set_ylabel("")
+axs[0, 0].set_ylabel("")
 axs[0, 1].hist(df1000['Maximum income'], bins=20, rwidth=0.9)
 axs[0, 1].set_title("1000")
-axs[1, 0].hist(df4000['Maximum income'], bins=20, rwidth=0.9)
-axs[1, 0].set_title("4000")
+axs[1, 0].hist(df2000['Maximum income'], bins=20, rwidth=0.9)
+axs[1, 0].set_title("2000")
 axs[1, 0].set_xlabel("Zysk")
-axs[1, 1].hist(df10000['Maximum income'], bins=20, rwidth=0.9)
-axs[1, 1].set_title("10000")
+axs[1, 1].hist(df4000['Maximum income'], bins=20, rwidth=0.9)
+axs[1, 1].set_title("4000")
 axs[1, 1].set_xlabel("Zysk")
 axs[-1, 0].set_xlabel('.', color=(0, 0, 0, 0))
 axs[-1, 0].set_ylabel('.', color=(0, 0, 0, 0))
