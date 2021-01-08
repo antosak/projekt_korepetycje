5aa9b57d970d7a5fa57863ba2b1dcf211bbac1ef
Michał Antos <antosak47@gmail.com>
2021-01-08 17:13:50 +0100

    the end is near

--- data_analysis.py
+++ data_analysis.py
@@ -20,8 +20,9 @@ def first_test():
     df10000 = pd.read_excel("Dat/RawData_iter_10000.xlsx", index_col=0)
 
     fig1, axs = plt.subplots(7, 3, figsize=(8.25, 11.75), sharey="row", sharex="col")
-    fig1.suptitle("Porównanie składowych i ostatecznej funkcji celu (1000 powtórzeń)", fontsize=16)
-    fig1.subplots_adjust(top=0.95, left=0.1)
+    fig1.suptitle("Porównanie składowych i ostatecznej funkcji celu", fontsize=16)
+    fig1.subplots_adjust(top=0.95, bottom=0.05)
+    fig1.text(0.02, 0.5, 'Liczba iteracji', va='center', rotation='vertical', fontsize=14)
 
     axs[0, 0].hist(df500['Maximum income'], bins=20, rwidth=szerokosc_paskow)
     axs[0, 0].set_ylabel("500")
@@ -66,18 +67,19 @@ def first_test():
 def second_test():
     data4000 = pd.read_excel("Dat/RawData_oneiter_4000.xlsx", index_col=0)
     data10000 = pd.read_excel("Dat/RawData_oneiter_10000.xlsx", index_col=0)
-    fig, axs = plt.subplots(1, 2, figsize=(10, 6), sharey='row')
+    fig, axs = plt.subplots(2, 1, figsize=(8.25, 6), sharey='row')
     fig.suptitle("Zachowanie się granicznych i średniego rozwiązania", fontsize=16)
+    fig.text(0.02, 0.5, 'Funkcja celu [PLN/h]', va='center', rotation='vertical', fontsize=14)
 
-    axs[0].plot(data4000["Minimum Fun"], color="red", label="Minimum")
+    axs[0].plot(data4000.iloc[10:]["Minimum Fun"], color="red", label="Minimum")
     axs[0].plot(data4000["Average Fun"], color="blue", label="Średnia")
     axs[0].plot(data4000["Maximum Fun"], color='green', label="Maksimum")
-    axs[0].set_ylabel("Funkcja celu [PLN/h]", fontsize=12)
-    axs[0].set_xlabel("Iteracja", fontsize=12)
-    axs[1].plot(data10000["Minimum Fun"], color="red", label="Minimum")
+    axs[0].grid()
+    axs[1].plot(data10000.iloc[13:]["Minimum Fun"], color="red", label="Minimum")
     axs[1].plot(data10000["Average Fun"], color="blue", label="Średnia")
     axs[1].plot(data10000["Maximum Fun"], color='green', label="Maksimum")
-    axs[1].set_xlabel("Iteracja", fontsize=12)
+    axs[1].set_xlabel("Iteracja", fontsize=14)
+    axs[1].grid()
     plt.legend(fontsize=16)
     plt.savefig("Plots & Charts/max average min.png")
     plt.show()
@@ -92,7 +94,7 @@ def third_test_hist():
     df100 = pd.read_excel("Dat/RawData_popsize100.xlsx", index_col=0)
 
     fig1, axs = plt.subplots(4, 3, figsize=(9.5, 6.75), sharey="row", sharex="col")
-    fig1.suptitle("Porównanie składowych i ostatecznej funkcji celu (1000 powtórzeń)", fontsize=16)
+    fig1.suptitle("Porównanie funkcji celu dla różnych wielkości populacji", fontsize=16)
     fig1.subplots_adjust(top=0.93, left=0.1)
     fig1.text(0.01, 0.5, 'Wielkość populacji', va='center', rotation='vertical', fontsize=14)
 
@@ -104,7 +106,7 @@ def third_test_hist():
     axs[2, 0].set_ylabel("75")
     axs[3, 0].hist(df100['Maximum income'], bins=20, rwidth=szerokosc_paskow)
     axs[3, 0].set_ylabel("100")
-    axs[3, 0].set_xlabel("Zysk [PLN]", fontsize=12)
+    axs[3, 0].set_xlabel("Przychód [PLN]", fontsize=12)
 
     axs[0, 1].hist(df25['Suboptimal teaching time'], bins=20, rwidth=szerokosc_paskow)
     axs[1, 1].hist(df50['Suboptimal teaching time'], bins=20, rwidth=szerokosc_paskow)
@@ -131,10 +133,10 @@ def third_test_plot():
     data3060 = pd.read_excel("Dat/RawData_popsize_2_75.xlsx", index_col=0)
     data4590 = pd.read_excel("Dat/RawData_popsize_2_100.xlsx", index_col=0)
 
-    fig, axs = plt.subplots(4, 1, figsize=(10, 15), sharex='col')
+    fig, axs = plt.subplots(4, 1, figsize=(8.25, 12), sharex='col')
     fig.suptitle("Wynik algorytmu dla różnych rozmiarów populacji", fontsize=18)
     fig.text(0.04, 0.5, 'Rozmiar populacji', va='center', rotation='vertical', fontsize=14)
-    fig.subplots_adjust(top=0.95)
+    fig.subplots_adjust(top=0.95, bottom=0.05)
 
     axs[0].plot(data1020.iloc[500:]["Minimum Fun"], color="red", label="Minimum")
     axs[0].plot(data1020.iloc[500:]["Average Fun"], color="blue", label="Średnia")
@@ -169,16 +171,17 @@ def third_test_plot():
     return fig
 
 
+# Plot with first 500
 def third_test_plot_vel_2():
     data1020 = pd.read_excel("Dat/RawData_popsize_2_25.xlsx", index_col=0)
     data2040 = pd.read_excel("Dat/RawData_popsize_2_50.xlsx", index_col=0)
     data3060 = pd.read_excel("Dat/RawData_popsize_2_75.xlsx", index_col=0)
     data4590 = pd.read_excel("Dat/RawData_popsize_2_100.xlsx", index_col=0)
 
-    fig, axs = plt.subplots(4, 1, figsize=(10, 15), sharex='col')
+    fig, axs = plt.subplots(4, 1, figsize=(8.25, 12), sharex='col')
     fig.suptitle("Wynik algorytmu dla różnych rozmiarów populacji", fontsize=18)
     fig.text(0.04, 0.5, 'Rozmiar populacji', va='center', rotation='vertical', fontsize=14)
-    fig.subplots_adjust(top=0.95)
+    fig.subplots_adjust(top=0.95, bottom=0.05)
 
     axs[0].plot(data1020["Minimum Fun"], color="red", label="Minimum")
     axs[0].plot(data1020["Average Fun"], color="blue", label="Średnia")
@@ -221,10 +224,10 @@ def fourth_test():
     data4590 = pd.read_excel("Dat/RawData_prob4590.xlsx", index_col=0)
     data00 = pd.read_excel("Dat/RawData_prob00.xlsx", index_col=0)
 
-    fig, axs = plt.subplots(5, 1, figsize=(10, 15), sharex='col')
-    fig.suptitle("Wynik algorytmu dla różnych częstotliwości występowania operatorów", fontsize=18)
-    fig.text(0.04, 0.5, 'Progi zmiany operatora krzyżowania, mutacji', va='center', rotation='vertical', fontsize=14)
-    fig.subplots_adjust(top=0.95)
+    fig, axs = plt.subplots(5, 1, figsize=(8.25, 12), sharex='col')
+    fig.suptitle("Wynik algorytmu dla różnych progów zmiany operatorów", fontsize=18)
+    fig.text(0.02, 0.5, 'Progi zmiany operatora krzyżowania, mutacji', va='center', rotation='vertical', fontsize=14)
+    fig.subplots_adjust(top=0.95, bottom=0.05)
 
     axs[0].plot(data1020.iloc[13:]["Minimum Fun"], color="red", label="Minimum")
     axs[0].plot(data1020["Average Fun"], color="blue", label="Średnia")
@@ -240,7 +243,6 @@ def fourth_test():
     axs[2].plot(data3060["Average Fun"], color="blue", label="Średnia")
     axs[2].plot(data3060["Maximum Fun"], color='green', label="Maksimum")
     axs[2].set_ylabel("1200, 2400")
-    axs[2].set_xlabel("Iteracja", fontsize=14)
     axs[2].grid()
     axs[3].plot(data4590.iloc[13:]["Minimum Fun"], color="red", label="Minimum")
     axs[3].plot(data4590["Average Fun"], color="blue", label="Średnia")
@@ -262,6 +264,7 @@ def fourth_test():
     return fig
 
 
+# Probability of crossover and mutation without first 500 iterations
 def fourth_test_vel2():
     data1020 = pd.read_excel("Dat/RawData_prob1020.xlsx", index_col=0)
     data2040 = pd.read_excel("Dat/RawData_prob2040.xlsx", index_col=0)
@@ -269,10 +272,10 @@ def fourth_test_vel2():
     data4590 = pd.read_excel("Dat/RawData_prob4590.xlsx", index_col=0)
     data00 = pd.read_excel("Dat/RawData_prob00.xlsx", index_col=0)
 
-    fig, axs = plt.subplots(5, 1, figsize=(10, 15), sharex='col')
-    fig.suptitle("Wynik algorytmu dla różnych częstotliwości występowania operatorów", fontsize=18)
-    fig.text(0.04, 0.5, 'Progi zmiany operatora krzyżowania, mutacji', va='center', rotation='vertical', fontsize=14)
-    fig.subplots_adjust(top=0.95)
+    fig, axs = plt.subplots(5, 1, figsize=(8.25, 12), sharex='col')
+    fig.suptitle("Wynik algorytmu dla różnych progów zmiany operatorów", fontsize=18)
+    fig.text(0.02, 0.5, 'Progi zmiany operatora krzyżowania, mutacji', va='center', rotation='vertical', fontsize=14)
+    fig.subplots_adjust(top=0.95, bottom=0.05)
 
     axs[0].plot(data1020.iloc[500:]["Minimum Fun"], color="red", label="Minimum")
     axs[0].plot(data1020.iloc[500:]["Average Fun"], color="blue", label="Średnia")
@@ -308,12 +311,14 @@ def fourth_test_vel2():
     return fig
 
 
-# Wpływ początkowej generacji rozwiązań na końcowe rozwiązanie
+# Influence of initial population on final solution
 def controversial_test():
     df4000 = pd.read_excel("Dat/RawData_iter_4000.xlsx", index_col=0)
 
-    fig, axs = plt.subplots(2, 1, figsize=(9.6, 10.6))
-    axs[0].set_title("Zależność końcowego najlepszego rozwiązania od początkowego najlepszego rozwiązania", fontweight='bold')
+    fig, axs = plt.subplots(2, 1, figsize=(8.25, 9))
+    fig.subplots_adjust(top=0.95, bottom=0.05)
+
+    axs[0].set_title("Końcowego najlepsze rozwiązanie a początkowe najlepsze rozwiązanie", fontweight='bold')
     axs[0].scatter(df4000["Initial maximum"], df4000["Final maximum"], s=5)
     axs[0].grid()
     z = np.polyfit(df4000["Initial maximum"], df4000["Final maximum"], 1)
@@ -325,7 +330,7 @@ def controversial_test():
     axs[0].set_xlabel("Początkowe najlepsze rozwiązanie [PLN/h]")
     axs[0].set_ylabel("Końcowe najlepsze rozwiązanie [PLN/h]")
 
-    axs[1].set_title("Zależność końcowego średniego rozwiązania od początkowego średniego rozwiązania", fontweight='bold')
+    axs[1].set_title("Końcowe średnie rozwiązanie a początkowe średnie rozwiązanie", fontweight='bold')
     axs[1].scatter(df4000["Initial average"], df4000["Final average"], s=5)
     axs[1].grid()
     z = np.polyfit(df4000["Initial average"], df4000["Final average"], 1)
@@ -346,16 +351,17 @@ def optimized_performance():
     data1020 = pd.read_excel("Dat/RawData_final_chyba.xlsx", index_col=0)
 
     fig, axs = plt.subplots(1, 1, figsize=(8, 6), sharex='col')
-    fig.suptitle("Wynik algorytmu dla optymlanych parametrów", fontsize=18)
+    fig.suptitle("Wynik algorytmu dla wybranych najlepszych parametrów", fontsize=18)
 
     axs.plot(data1020.iloc[500:]["Minimum Fun"], color="red", label="Minimum")
     axs.plot(data1020.iloc[500:]["Average Fun"], color="blue", label="Średnia")
     axs.plot(data1020.iloc[500:]["Maximum Fun"], color='green', label="Maksimum")
-    axs.set_ylabel("Funkcja celu [PLN/h]")
+    axs.set_ylabel("Funkcja celu [PLN/h]", fontsize=14)
     axs.set_ylim([27, 44])
     axs.grid()
+    axs.set_xlabel("Iteracja", fontsize=14)
 
-    plt.legend(fontsize=16)
+    plt.legend(loc="lower right", fontsize=16)
 
     plt.savefig("Plots & Charts/optymalność.png")
     plt.show()
@@ -363,4 +369,4 @@ def optimized_performance():
     return fig
 
 
-a = optimized_performance()
+a = third_test_hist()
